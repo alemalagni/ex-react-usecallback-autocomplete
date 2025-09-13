f1ebc1a565a6efd4b35e58a496efb724e949075a
alemalagni <alemalagni@gmail.com>
2025-09-13 12:51:46 +0200

    feat: add JSX

---
+++ jsx/ReactApp.jsx
@@ -0,0 +1,10 @@
+const root = createRoot(document.querySelector(".root"));
+root.render(<ReactMain />);
+
+function ReactApp() {
+    return (
+        <div>
+            <input type='text' />
+        </div>
+    )
+}
